7990bd6c4d94b88b4dba836edc7b737a4b30388b
Eugene Song <yougene42193@gmail.com>
2019-06-06 11:29:26 -0400

    Initial commit

---
+++ Queue.js
@@ -0,0 +1,87 @@
+'use strict';
+const { Stack } = require('./Stack');
+
+class _Node {
+    constructor(value) {
+        this.value = value,
+        this.next = null,
+        this.prev = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+    }
+    enqueue(data) {
+        const node = new _Node(data);
+
+        if (this.first === null) {
+            this.first = node;
+        }
+        if (this.last) {
+            node.next = this.last;
+            this.last.prev = node;
+        }
+        this.last = node;
+    }
+    dequeue() {
+        if (this.first === null) {
+            return;
+        }
+        const node = this.first;
+        this.first = node.prev;
+
+        if (node === this.last) {
+            this.last = null;
+        }
+        return node.value;
+    }
+}
+
+function peek(q) {
+    console.log(q.first.value);
+}
+
+function display(q) {
+    let current = q.first;
+    while(current) {
+        console.log(current.value);
+        current = current.next;
+    }
+}
+
+class StackQueue {
+    constructor() {
+        this.firstStack = new Stack;
+        this.lastStack = new Stack;
+    }
+    enqueue(value) {
+        this.firstStack.push(value)
+    }
+    dequeue() {
+        while (this.firstStack.top) {
+            this.lastStack.push(this.firstStack.pop());
+        }
+        const result = this.lastStack.pop()
+        while (this.lastStack.top) {
+            this.firstStack.push(this.lastStack.pop())
+        }
+        console.log(result);
+    }
+}
+
+function main() {
+    const starTrekQ = new Queue();
+    starTrekQ.enqueue('Kirk');
+    starTrekQ.enqueue('Spock');
+    starTrekQ.enqueue('Uhura');
+    starTrekQ.enqueue('Sulu');
+    starTrekQ.enqueue('Checkov');
+    starTrekQ.dequeue();
+    starTrekQ.dequeue();
+    display(starTrekQ);
+}
+
+main();
---
+++ Stack.js
@@ -0,0 +1,109 @@
+'use strict';
+
+class _Node {
+    constructor(data, next) {
+        this.data = data;
+        this.next = next;
+    }   
+}
+
+class Stack {
+    constructor() {
+        this.top = null;
+    }
+    push(data) {
+        if (this.top === null) {
+            this.top = new _Node(data, null);
+            return this.top;
+        }
+
+        const node = new _Node(data, this.top);
+        this.top = node;
+    }
+
+    pop() {
+        const node = this.top;
+        this.top = node.next;
+        return node.data;
+    }
+}
+
+function peek(stack) {
+    if(!stack.top) {
+        return;
+    }
+    else {
+        console.log(stack.top.data);
+    }
+}
+
+function display(stack) {
+    let counter = stack.top;
+    while(counter) {
+        console.log(counter.data);
+        counter = counter.next;
+    }
+}
+
+function is_palindrome(s) {
+    s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    let reverse = new Stack();
+    for(let i = 0; i < s.length; i++) {
+        reverse.push(s.charAt(i));
+    }
+    for(let j = 0; j < s.length; j++) {
+        if(s[j] === reverse.pop()) continue;
+        else return false;
+    }
+    return true;
+}
+
+function match(str) {
+    let newStack = new Stack();
+    let counter = 0;
+    for(let i = 0; i < str.length; i++) {
+        if(str[i] === '(') {
+            newStack.push(str[i]);
+            counter++;
+        }
+        if(str[i] === ')' && !newStack.top) {
+            return new Error (`Missing an open parenthesis at ${counter}`)
+        }
+        if(str[i] === ')') {
+            newStack.pop();
+            counter++;
+        }
+        if(newStack.top === '(') {
+            return new Error(`Missing an closing parenthesis at ${counter}`)
+        }
+    }
+    return true;
+}
+
+function sort(stack) {
+    let sorted = new Stack();
+    while(stack.top) {
+        let saved = stack.pop();
+        while(sorted.top && sorted.top.data > saved) {
+            stack.push(sorted.pop());
+        }
+        sorted.push(saved);
+    }
+    while(sorted.top) {
+        stack.push(sorted.pop())
+    }
+    return stack;
+}
+
+function main() {
+    const starTrek = new Stack();
+    starTrek.push('Kirk');
+    starTrek.push('Spock');
+    starTrek.push('McCoy');
+    starTrek.push('Scotty');
+    starTrek.pop();
+    starTrek.pop();
+    display(starTrek);
+}
+
+main();
